b6cd38a434ddad20aa526769fd1dbf29c7812721
Sega Diop <contact@madi-corp.net>
2016-08-04 15:03:06 +0000

    ok

--- config/initializers/paperclic.rb
+++ config/initializers/paperclic.rb
@@ -1,4 +1,4 @@
-Paperclip.options[:command_path] = 'C:\Users\A621217\Documents\Perso\Dakarclic\GnuWin32\bin'
+#Paperclip.options[:command_path] = 'C:\Users\A621217\Documents\Perso\Dakarclic\GnuWin32\bin'
 Paperclip.options[:content_type_mappings] = {
     :jpg => "image/jpeg",
     :png => "image/png",
