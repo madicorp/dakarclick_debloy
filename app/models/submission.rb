2c429cb042775b1095f2d274c19a9f4d5c4f7e36
Sega Diop <contact@madi-corp.net>
2016-08-08 17:24:38 +0000

    change path

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,5 +1,7 @@
 class Submission < ActiveRecord::Base
     belongs_to :user
-    has_attached_file :image
+    has_attached_file :image,
+                      :path =>   ":rails_root/upload/:rails_env/:class/:attachment/:id_partition/:style/:filename",
+                      :url => "/upload/:rails_env/:class/:attachment/:id_partition/:style/:filename"
     validates_attachment_content_type :image, :content_type => [/\Aimage/, 'application/octet-stream']
 end
