0f227c6da4298b53fc150228264d25d992497208
Sega Diop <contact@madi-corp.net>
2016-08-13 17:09:55 +0000

    host

--- config/initializers/paperclic.rb
+++ config/initializers/paperclic.rb
@@ -1,7 +1,7 @@
 #Paperclip.options[:command_path] = 'C:\Users\A621217\Documents\Perso\Dakarclic\GnuWin32\bin'
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_host_name] = 's3.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
 Paperclip.options[:content_type_mappings] = {
     :jpg => "image/jpeg",
     :png => "image/png",
