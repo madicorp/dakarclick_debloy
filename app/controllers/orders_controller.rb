6bb881217c8e9818a6a6a1730801c8bfd7e38ff4
Sega Diop <contact@madi-corp.net>
2016-09-02 13:06:45 +0000

    paydunya error

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,9 +7,9 @@ class OrdersController < ApplicationController
 
   # # GET /orders
   # # GET /orders.json
-  # def index
-  #   @orders = Order.all
-  # end
+  def index
+    redirect_to root_path
+  end
   #
   # # GET /orders/1
   # # GET /orders/1.json
@@ -18,7 +18,7 @@ class OrdersController < ApplicationController
 
   # GET /orders/new
   def new
-
+    redirect_to root_path if current_user.nil?
     @order = Order.new
     @order.quantity = params[:qte] unless params[:qte].nil?
     @order.total_ttc = params[:total] unless params[:total].nil?
@@ -30,9 +30,10 @@ class OrdersController < ApplicationController
   # end
 
   def create
-    p order_params
+    redirect_to root_path if current_user.nil?
     @order = Order.new order_params
-    @order.status= "Pending"
+    @order.status= "pending"
+    @order.user_id = current_user.id
     if @order.save
       case @order.payment_method
         when "paydunya"
@@ -53,103 +54,6 @@ class OrdersController < ApplicationController
             puts invoice.status
             puts invoice.response_text
           end
-        when "card"
-          Money.default_bank = Money::Bank::GoogleCurrency.new
-          card = card_params
-          p  '%.2f' % @order.total_ttc.to_money(:XOF).exchange_to(:EUR)
-          p ('%.2f' % (100.to_money(:XOF).exchange_to(:EUR)* @order.quantity))
-          # ###Payment
-          # A Payment Resource; create one using
-          # the above types and intent as `sale or `authorize`
-          @payment = PayPal::SDK::REST::Payment.new({
-                                                        :intent => "sale",
-                                                        :payer => {
-                                                            :payment_method => "credit_card",
-                                                            :funding_instruments => [{
-                                                                                         :credit_card => {
-                                                                                             :type => card[:type],
-                                                                                             :number => card[:number].tr(" ", ""),
-                                                                                             :expire_month => card[:month],
-                                                                                             :expire_year => card[:year],
-                                                                                             :cvv2 => card[:cvc],
-                                                                                             :first_name => card[:firstname],
-                                                                                             :last_name => card[:lastname],
-                                                                                         }
-                                                                                     }]
-                                                        },
-                                                        :transactions =>  [{
-                                                                               # Item List
-                                                                               :item_list => {
-                                                                                   :items => [{
-                                                                                                  :name => "Unités DakarClick",
-                                                                                                  :currency => "EUR",
-                                                                                                  :price => '%.2f' % 100.to_money(:XOF).exchange_to(:EUR),
-                                                                                                  :quantity => @order.quantity}]},
-                                                                               :amount =>  {
-                                                                                   :total =>  '%.2f' % (100.to_money(:XOF).exchange_to(:EUR)* @order.quantity),
-                                                                                   :currency =>  "EUR"
-                                                                               },
-                                                                               :description =>  "This is the payment transaction description."
-                                                                           }]
-                                                    })
-          # Create Payment and return status( true or false )
-          if @payment.create
-            p "Payment[#{@payment.id}] created successfully"
-            unit = @payment.transactions[0].item_list.items[0].quantity
-            user = current_user
-            if user.units.nil?
-              user.units =0
-            end
-
-            user.units  += unit.to_i
-            if user.save
-              redirect_to confirm_path
-            end
-
-          else
-            # Display Error message
-            p "Error while creating payment:"
-            p @payment.error.inspect
-          end
-          redirect_to root_path
-        when "paypal"
-          Money.default_bank = Money::Bank::GoogleCurrency.new
-          @payment = PayPal::SDK::REST::Payment.new({
-                                                        :intent =>  "sale",
-                                                        :payer =>  {
-                                                            :payment_method =>  "paypal"
-                                                        },
-                                                        :redirect_urls => {
-                                                            :return_url => "http://localhost:3000/confirm/paypal",
-                                                            :cancel_url => "http://localhost:3000/confirm/paypal"
-                                                        },
-                                                        :transactions =>  [{ # Item List
-                                                                             :item_list => {
-                                                                                 :items => [{
-                                                                                                :name => "Unités DakarClick",
-                                                                                                :currency => "EUR",
-                                                                                                :price => '%.2f' % 100.to_money(:XOF).exchange_to(:EUR),
-                                                                                                :quantity => @order.quantity}]},
-                                                                             :amount =>  {
-                                                                                 :total =>  '%.2f' % (100.to_money(:XOF).exchange_to(:EUR)* @order.quantity),
-                                                                                 :currency =>  "EUR"
-                                                                             },
-                                                                             :description =>  "This is the payment transaction description."
-                                                                           }]
-                                                    })
-
-          # Create Payment and return status
-          if @payment.create
-            # Redirect the user to given approval url
-            @redirect_url = @payment.links.find{|v| v.method == "REDIRECT" }.href
-            redirect_to @redirect_url.to_s.split(": ").last.tr("\"","")
-            p "Payment[#{@payment.id}]"
-            p "Redirect: #{@redirect_url}"
-          else
-            p @payment.error.inspect
-          end
-        else
-          redirect_to root_path
       end
     end
 
