47f72cbf05d3ca7ae36ef25d9d87689ff3cb284e
Sega Diop <contact@madi-corp.net>
2016-09-03 11:50:57 +0000

    skip confirm

--- app/controllers/confirm_controller.rb
+++ app/controllers/confirm_controller.rb
@@ -1,6 +1,7 @@
 require 'open-uri'
 require 'paypal-sdk-rest'
 class ConfirmController < ApplicationController
+    skip_before_action :verify_authenticity_token, :only => [:paydunya]
     def index
         redirect_to root_path
     end
